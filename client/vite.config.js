6c9d098109fd523b08d8c3baf0bd3f46644966fe
mikhenaff <109875471+mikhenaff@users.noreply.github.com>
2024-04-01 12:30:39 +0200

    change proxy in vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "kaamelott-app-server.vercel.app/api",
+      "/api": "kaamelott-app-client.vercel.app/api",
     },
   },
   plugins: [react()],
